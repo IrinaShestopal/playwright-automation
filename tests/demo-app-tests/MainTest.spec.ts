aaaaca87a41eab547762ba8689f84668219046a3
ishestopal <iryna.shestopal@dataart.com>
2026-09-08 12:32:24 +0100

    verify click button

--- tests/demo-app-tests/MainTest.spec.ts
+++ tests/demo-app-tests/MainTest.spec.ts
@@ -1,7 +1,15 @@
 import { test, expect } from '@playwright/test';
 
-test('verify deployed demo application', async ({ page }) => {
+test('verify app heading', async ({ page }) => {
   await page.goto('https://demo-app-for-playwright-automation.onrender.com');
 
   await expect(page.getByRole('heading', { name: 'Hi my love' })).toBeVisible();
+});
+
+test('verify click button', async ({ page }) => {
+  await page.goto('https://demo-app-for-playwright-automation.onrender.com'); 
+  
+  await page.getByRole('button', { name: 'Click me!', exact: true }).click();
+
+  await expect(page.locator('#message')).toContainText('How are you?!');
 });
